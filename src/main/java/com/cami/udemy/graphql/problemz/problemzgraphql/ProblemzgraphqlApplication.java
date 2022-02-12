63f28d0e8120abd1ff88dc7638ef4face15a6815
CRatiu <c.ratiu@ftapi.com>
2022-02-12 15:43:39 +0100

    Initial commit

---
+++ src/main/java/com/cami/udemy/graphql/problemz/problemzgraphql/ProblemzgraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.cami.udemy.graphql.problemz.problemzgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProblemzgraphqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProblemzgraphqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cami/udemy/graphql/problemz/problemzgraphql/ProblemzgraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cami.udemy.graphql.problemz.problemzgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProblemzgraphqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
